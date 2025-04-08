4b2be1bd50b447ae1feddda784c4f23162024e56
NemoDragon <adamtokarz.kol@gmail.com>
2025-04-08 22:16:13 +0200

    created frontend and backend working areas

---
+++ PuzzleAppBackend/src/main/java/com/example/puzzleappbackend/PuzzleAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.puzzleappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuzzleAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuzzleAppBackendApplication.class, args);
+	}
+
+}

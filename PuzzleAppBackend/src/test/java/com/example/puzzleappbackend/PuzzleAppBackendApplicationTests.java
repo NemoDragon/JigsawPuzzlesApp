82f68f031ff3aba1154e543bd8b812481220f070
NemoDragon <adamtokarz.kol@gmail.com>
2025-04-08 22:16:13 +0200

    created frontend and backend working areas

---
+++ PuzzleAppBackend/src/test/java/com/example/puzzleappbackend/PuzzleAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.puzzleappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuzzleAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

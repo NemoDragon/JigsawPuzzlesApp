e4d233b525c99ae849087c54d697559c24fbb12e
NemoDragon <adamtokarz.kol@gmail.com>
2025-04-09 21:54:41 +0200

    backend puzzle module

---
+++ PuzzleAppBackend/src/main/java/puzzle/PuzzleRepository.java
@@ -0,0 +1,8 @@
+package puzzle;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PuzzleRepository extends JpaRepository<Puzzle, Integer> {
+}

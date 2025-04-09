dee8c2e4c1c2d117a0234658f938663cc855557e
NemoDragon <adamtokarz.kol@gmail.com>
2025-04-10 01:10:07 +0200

    connecting backend puzzles to frontend

---
+++ PuzzleAppFrontend/puzzle-app/src/app/components/main/main.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Puzzle } from '../../models/puzzle.model';
+import { PuzzleService } from '../../services/puzzle.service';
+import { Console } from 'console';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css',
+})
+export class MainComponent implements OnInit {
+  puzzles: Puzzle[] = [];
+
+  constructor(private puzzleService: PuzzleService) {}
+
+  ngOnInit() {
+    this.puzzleService.getPuzzles().subscribe((response) => {
+      this.puzzles = response.puzzles;
+      console.log(this.puzzles);
+    });
+  }
+}

3bd7975d89d9885ad1d9af08acf2626c55033f61
NemoDragon <adamtokarz.kol@gmail.com>
2025-04-09 22:38:02 +0200

    backend user module

---
+++ PuzzleAppBackend/src/main/java/user/User.java
@@ -0,0 +1,32 @@
+package user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "user_id")
+    private Integer userId;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "country")
+    private String country;
+}
---
+++ PuzzleAppBackend/src/main/java/user/UserService.java
@@ -0,0 +1,49 @@
+package user;
+
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Integer userId) {
+        return userRepository.findById(userId);
+    }
+
+    public Integer addUser(User user) {
+        User savedUser = User.builder()
+                .userId(user.getUserId())
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .password(user.getPassword())
+                .country(user.getCountry())
+                .build();
+        return userRepository.save(savedUser).getUserId();
+    }
+
+    public User updateUser(Integer userId, User user) {
+        User foundUser = userRepository.findById(userId).orElseThrow(EntityNotFoundException::new);
+        foundUser.setUsername(user.getUsername());
+        foundUser.setEmail(user.getEmail());
+        foundUser.setPassword(user.getPassword());
+        foundUser.setCountry(user.getCountry());
+        return userRepository.save(foundUser);
+    }
+
+    public void deleteUser(Integer userId) {
+        if (!userRepository.existsById(userId)) {
+            throw new EntityNotFoundException();
+        }
+        userRepository.deleteById(userId);
+    }
+}
